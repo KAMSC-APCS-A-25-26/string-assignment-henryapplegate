d61f16b2a0388be5ca6db649a73533fa6e6fe9c8
henryapplegate <ha7applegate@gmail.com>
2025-10-10 19:51:55 -0400

    Update WordCensor.java

--- src/main/java/WordCensor.java
+++ src/main/java/WordCensor.java
@@ -2,22 +2,14 @@ import java.util.Scanner;
 
 public class WordCensor {
     public static void main(String[] args) {
-        // TODO: Read a full sentence from the user using Scanner.nextLine()
         Scanner scanner = new Scanner(System.in);
         String[] bannedWords = {"dang", "nuts", "oops", "yikes"};
         String input = scanner.nextLine();
 
         for (String word : bannedWords) {
-            // Replace whole words only, not substrings of other words
-            input = input.replaceAll(word, "***");
+            input = input.replaceAll("\\b" + word + "\\b", "***");
         }
 
         System.out.println("Censored: " + input);
-
-        // TODO: Replace each banned word with exactly ***: dang, nuts, oops, yikes
-
-        // TODO: Print the result prefixed with: Censored:
-        // NOTE: Tests require exactly three asterisks (no ****) for every replacement
-
     }
 }
