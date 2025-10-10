4851cedd31b4976cf1f0e03a0e66c93683cf176e
henryapplegate <ha7applegate@gmail.com>
2025-10-10 19:22:39 -0400

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -4,12 +4,26 @@ public class PalindromeChecker {
     public static void main(String[] args) {
         // TODO: Create a Scanner and read a full line from the user with nextLine().
         //       Keep the original input string so you can print it in the final message.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String input = sc.nextLine();
 
         // TODO: Build a lowercase version of the input for checking (case-insensitive comparison).
         //       (You don't need to remove spaces/punctuation for the current tests.)
+        String inputLower = input.toLowerCase();
+        String inputBack = "";
 
         // TODO: Determine whether the lowercase string reads the same forwards and backwards.
         //       Tip: compare it to a reversed copy of itself.
+        for (int i = inputLower.length() - 1; i >= 0; i--){
+            inputBack += input.charAt(i);
+        }
+
+        if (inputBack.equals(inputLower)){
+            System.out.println(input + " is a palindrome.");
+        }
+        else
+            System.out.println(input + " is not a palindrome.");
 
         // TODO: Treat single-character inputs as palindromes (the above check will already handle this).
 
